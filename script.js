e12e8d85321e0a5d4597a98edd516e803bf0f591
SHADIL VV <shadilshad508@gmail.com>
2025-09-08 21:06:37 +0530

    starting

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function pari(a,b){
 }
 let result= pari(5,2);
 console.log(result);
+
+
+const met = (who) => "hi, did u meet " + who + "yesterday?";
+console.log(met("shadil"));
+
+let data= "njan alle";
+function happy(solllu){
+    happy="ntho";
+    console.log(data);
+    console.log(happy);
+}
+happy();
+
+
+
+const obj = {
+  name: "Shadu",
+  greet: function() {
+    console.log(this.name);
+  }
+};
+
+obj.greet(); // Shadu

3f73556300f5e68892875308367a9ac7e5fd9e9c
SHADIL VV <shadilshad508@gmail.com>
2025-09-11 22:40:33 +0530

    Internship company tasks

--- he.js
+++ he.js
@@ -12,4 +12,18 @@ console.log(says("Aaro"));
 
 
 const mele=(aah) => "Hey! Am working in mele " + aah;
-console.log(mele(technology));
+console.log(mele("technology"));
+
+
+let xy=(sandy)=>"Sandy is the name of " + sandy;
+console.log(xy("sandhiya"));
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(5,7));
+
+const multi=(b,c)=>{
+    return "B x C is " + ( b*c)
+};
+console.log(multi(5,7));

e36811ec75f26fe8623d8e4603285d307cc2813e
SHADIL VV <shadilshad508@gmail.com>
2025-09-11 22:11:28 +0530

    Internship company tasks

--- he.js
+++ he.js
@@ -1,2 +1,15 @@
 let x=name;
 console.log(x);
+
+function greenperson(name){
+    console.log("hello world " + name);    
+}
+greenperson("shadil");
+
+const says=(name)=>"hi " + name;
+console.log(says("Aaro"));
+
+
+
+const mele=(aah) => "Hey! Am working in mele " + aah;
+console.log(mele(technology));
